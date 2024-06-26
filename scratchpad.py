e61774e6042c09acd2bbfa6b689b2c01b67ef08b
rminkler1 <robertminkler@gmail.com>
2024-06-25 21:48:00 -0700

    scratchpad

--- scratchpad.py
+++ scratchpad.py
@@ -1,12 +1,14 @@
-# list.append(element) is used to add an element to the end of a list.
-# list.count() returns the number of elements in a list
-# list.index(element) and str.index(element) return the index of the element in the string or list.
-# list.sort() sorts the list in place.
-# str().join(sequence of strings) joins the list or tuple of strings together, using the string as a separator.
+names = ['Robert', 'John', 'Sam', 'Barbara', 'Jacob', 'Jeremiah', 'Patricia', 'William', 'Robert']
 
-names = ['Robert', 'John', 'Sam', 'Barbara', 'Jacob', 'Jeremiah', 'Patricia', 'William']
+for index, name in enumerate(names):
+    print(index, name)
 
-empty_list = []
+    # Stop printing the next name when we reach the end of the list to avoid an IndexError
+    if index < (len(names) - 1):
+        print(f'The next name is {names[index + 1]}\n')
+    else:
+        print('You have reached the end of the list.')
 
-
-
+student_scores = [("Julie", 99), ("Josh", 94), ("Shelby", 75)]
+julie_name, julie_score = student_scores[0]
+print(f"Julie’s score is: {julie_score}")
