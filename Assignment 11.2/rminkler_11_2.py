07ae188c568cd8d266bdaf4d20d7ffa0bc4947cb
robertminkler <robertminkler@gmail.com>
2024-07-21 17:04:51 -0700

    assignment 11.2

--- Assignment 11.2/rminkler_11_2.py
+++ Assignment 11.2/rminkler_11_2.py
@@ -1,17 +1,65 @@
-# Module 11.2 Assignment (50 points)
-# Create a program with a recursive function that accepts an integer argument, n, and prints the number of 1 up to and including n. Then, write a non-recursive method that takes an integer argument, n, and prints the number of 1 up to and including n.
-#
-#     In your code documentation include an explanation of each functions approach to solving the problem.
-#     Include test code that will not allow a negative or 0 value.
-#     In your display, include which function is being invoked at both the start and end of the output.
-#
-# Test your program until it works, and the output looks readable and understandable. Add the necessary documentation as described in Documentation Requirements, and then attach your .py file(s) to this assignment.
-#
-# Assignment Requirements and Grading:
-#
-#     This assignment is due by Sunday, 11:59 p.m., CST.
-#     Add the necessary documentation as described in Documentation Requirements.
-#     Submit your .py file(s) by clicking on the assignment link above, then scroll down to the Upload Files section and click on Browse Local Files. Select your assignment file, add any comments as appropriate, and then click on Submit.
-#     To view or print the grading rubric for this assignment, click on the following link: Programming Rubric.
-#
-# (50 points)
+# Robert Minkler
+# July 21, 2024
+# Module 11.2
+
+# Create a recursive function to count from 1 to n
+# Create a non-recursive function to count from 1 to n
+
+def recursive_count(n: int) -> None:
+    """Count from 1 to n using a recursive function.
+    Count starts at n, then calls the function again, reducing n by one.
+    Once the count reaches 1, it prints 1 then continues in reverse,
+    printing n until the original entered value for n is reached.
+    """
+    if n == 1:  # define tha base case. Stop counting down at 1. Print 1
+        print(n)
+    elif n < 1:  # Return Error if passed a negative number or zero.
+        print("Please supply a number above 1.")
+    else:
+        # Recursive case. Run recursive_count function again with the next lowest number.
+        recursive_count(n - 1)
+        # Once complete print n. Last in first out.
+        # The last in will be 1, first in will be n
+        print(n)
+
+
+def looping_count(n: int) -> None:
+    """Count from 1 to n using a looping function."""
+    # loop through a range starting at one and ending at n
+    for num in range(1, n + 1):
+        # output the number from the range.
+        print(num)
+
+
+def main() -> None:
+    """Get a number from the user.
+    Count from 1 to that number recursively.
+    Then again using a loop.
+    """
+    print('Count from one to the number you enter.')
+
+    # Request user input of a number.
+    # Verify the entry is a positive int greater than zero before continuing.
+    # Verify the number is less than 999 to avoid hitting maximum recursion depth.
+    while not (num := input('Enter a number: ')).isdigit() or int(num) <= 0 or int(num) >= 999:
+        print('That is not a valid positive number.')
+        print('Enter a whole number between 1 and 998.')
+
+    # convert the string to an int.
+    num = int(num)
+
+    # call the recursive function
+    print('Begin Recursive Count Function:')
+    recursive_count(num)
+    print('End Recursive Count.')
+
+    print()     # print space to separate the counts.
+
+    # call the looping function
+    print('Begin Looping Count Function:')
+    looping_count(num)
+    print('End Looping Count.')
+
+
+if __name__ == '__main__':
+    main()
--- scratchpad.py
+++ scratchpad.py
@@ -1,15 +1,104 @@
-# Creating a list
-my_list = [1, 2, 3, 4, 5]
+# recursion
 
-# Creating a blank list to append items to
-my_list2 = list()
-my_list2.append(1)
+def main():
+    # Request a number from the user. Assign it to num.
+    # Validate that all characters are numbers. If not, show error msg and repeat.
+    while not (num := input('Enter a number: ')).isdigit():
+        print(f'{num} is not a positive whole number.')
 
-# Creating a set
-my_set = {0, 1, 2, 3, 4, 5, True, False}
+    num = int(num)
+    print_numbers(num)
 
-# Creating a blank set to add items to
-my_set2 = set()
-my_set2.add(1)
+    print(multiply(num, 10))
 
-print(my_set)
+    asterisks(num)
+
+    print('largest', largest([1, 2, 3, 4, 5, 6]))
+
+    print(f'sum list: {sum_list([1,2,3,4,5, 100])}')
+
+    print(f'sum of numbers: {sum_of_numbers(num)}')
+
+    print(power(5, 5))
+
+
+def print_numbers(n):
+    if n != 0:
+        print_numbers(n - 1)
+        print(n)
+
+
+def multiply(x, y):
+    if x == 0 or y == 0:
+        return 0
+    else:
+        return x * multiply(x, y - 1)
+
+
+def asterisks(n):
+    if n == 0:
+        return
+    else:
+        asterisks(n - 1)
+        print("*" * n)
+
+
+# 4. Largest List ItemDesign a function that accepts a list as an argument and returns the largest value in the list.
+# The function should use recursion to find the largest item.
+
+
+def largest(list, max_val=0):
+    if not list:
+        return max_val
+    else:
+        return largest(list[1:], max(list[0], max_val))
+
+# 5. Recursive List SumDesign a function that accepts a
+# list of numbers as an argument. The function should recursively calculate the sum of all the numbers in the list
+# and return that value.
+
+
+def sum_list(list):
+    if not list:
+        return 0
+    else:
+        return list[0] + sum_list(list[1:])
+
+
+# 6. Sum of NumbersDesign a function that accepts an integer argument and returns the sum of
+# all the integers from 1 up to the number passed as an argument. For example, if 50 is passed as an argument,
+# the function will return the sum of 1, 2, 3, 4, . . . 50. Use recursion to calculate the sum.
+
+
+def sum_of_numbers(num):
+    if num == 0:
+        return 0
+    else:
+        return num + sum_of_numbers(num - 1)
+
+# 7. Recursive Power
+# MethodDesign a function that uses recursion to raise a number to a power. The function should accept two arguments:
+# the number to be raised, and the exponent. Assume the exponent is a nonnegative integer.
+
+
+def power(x, y):
+    if y == 1:
+        return x
+    else:
+        return x * power(x, y - 1)
+
+# 8. Ackermann’s
+# FunctionAckermann’s Function is a recursive mathematical algorithm that can be used to test how well a system
+# optimizes its performance of recursion. Design a function , which solves Ackermann’s function. Use the following
+# logic in your function:Once you’ve designed your function, test it by calling it with small values for  and .
+
+
+
+if __name__ == '__main__':
+    main()
+
+#
+
+
+
+
